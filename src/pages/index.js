1526b893e5f6d488f3190aa250e9757b24277ae7
OODE Studios <oodestudios@example.com>
2025-10-26 15:46:11 +0100

    Use relative paths for homepage redirect and logo href

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,7 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import {Redirect} from '@docusaurus/router';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
-  // Redirect homepage to documentation with baseUrl
-  return <Redirect to={`${siteConfig.baseUrl}docs/getting-started/introduction`} />;
+  // Redirect homepage to documentation
+  return <Redirect to="docs/getting-started/introduction" />;
 }
